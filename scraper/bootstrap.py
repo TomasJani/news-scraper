0e72b9d2bd73277b1fb00fb5c4e46dfbcad2f6af
Tomas Janicek <xjanicek@fi.muni.cz>
2020-01-30 13:26:17 +0100

    Wrong Creation Order

--- scraper/bootstrap.py
+++ scraper/bootstrap.py
@@ -10,11 +10,12 @@ yesterday_time = (datetime.now() - timedelta(days=1)).strftime("%b_%d_%Y")
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-logging.basicConfig(filename=f'scraper/logs/{today_time}.log',
-                    format='%(asctime)s :: %(filename)s :: %(levelname)s = %(message)s',
-                    level=logging.INFO)
-
 for directory in ['scraper/data', 'scraper/logs', 'scraper/data/dennik_n', 'scraper/data_hlavne_spravy',
                   'scraper/data_plus_7_dni', 'scraper/data/sme', 'scraper/data/zem_a_vek']:
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+logging.basicConfig(filename=f'scraper/logs/{today_time}.log',
+                    format='%(asctime)s :: %(filename)s :: %(levelname)s = %(message)s',
+                    level=logging.INFO)
+
